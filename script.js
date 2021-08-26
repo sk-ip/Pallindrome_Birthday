ea5f92929a158ec7a5c395f943d7c82cf47bc8b8
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2021-08-26 20:24:10 +0530

    script: Add method for calculating pallindrome

---
+++ script.js
@@ -0,0 +1,158 @@
+function reverseStr(str) {
+  var charList = str.split('');
+  var reverListChar = charList.reverse();
+  var reverseStr = reverListChar.join('');
+  return reverseStr;
+}
+
+function isPallindrome(str) {
+  return reverseStr(str) === str;
+}
+
+function convertDateToString(date) {
+  var dateStr = {
+    day: "",
+    month: "",
+    year: "",
+  }
+
+  if(date.day < 10) {
+    dateStr.day = '0'+date.day;
+  } else {
+    dateStr.day = date.day.toString();
+  }
+
+  if(date.month < 10) {
+    dateStr.month = '0'+date.month;
+  } else {
+    dateStr.month = date.month.toString();
+  }
+
+  dateStr.year = date.year.toString();
+
+  return dateStr;
+}
+
+function getAllDateFormats(date) {
+  var dateStr = convertDateToString(date);
+
+  var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+  var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+  var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+
+  var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+  var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+  var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd]
+}
+
+function checkPallindromeForAllDateFormats(date) {
+  var listOfPallindromes = getAllDateFormats(date);
+
+  var flag = false;
+
+  for (i=0; i<listOfPallindromes.length; i++) {
+    if(isPallindrome(listOfPallindromes[i])) {
+      flag = true;
+      break
+    }
+  }
+
+  return flag
+}
+
+function isLeapYear(year) {
+  if(year % 400 === 0) {
+    return true;
+  }
+  if(year % 100 === 0) {
+    return false;
+  }
+  if (year % 4 === 0) {
+    return true;
+  }
+  return false
+}
+
+function getNextDate(date) {
+  var day = date.day +  1;
+  var month = date.month;
+  var year = date.year;
+
+  var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if(month === 2) {
+    // check for leap year
+    if (isLeapYear(year)) {
+      if(day > 29) {
+        day = 1;
+        month += 1;
+      }
+    } else {
+      if(day > 28) {
+        day = 1;
+        month += 1;
+      }
+    }
+  } else {
+    if(day > daysInMonth[month-1]) {
+      day = 1;
+      month += 1;
+    }
+  }
+
+  if(month > 12) {
+    month = 1;
+    year += 1;
+  }
+
+  return {
+    day: day,
+    month: month,
+    year: year,
+  }
+}
+
+function getNextPallindromeDate(date) {
+  var count = 0;
+  var nextDate = getNextDate(date);
+
+  while(1) {
+    count += 1;
+    if(checkPallindromeForAllDateFormats(nextDate)){
+      break
+    } else {
+      nextDate = getNextDate(nextDate);
+    }
+  }
+  return [count, nextDate];
+}
+
+var bdate = document.querySelector("#birth-date");
+var showBtnRef = document.querySelector("#show-btn");
+var resultRef = document.querySelector("#result");
+
+function clickHandler() {
+  var bdayStr = bdate.value;
+
+  if(bdayStr !== "") {
+    var listOfDate = bdayStr.split('-');
+    var date = {
+      day: Number(listOfDate[2]),
+      month: Number(listOfDate[1]),
+      year: Number(listOfDate[0]),
+    }
+
+    var isPallindrome = checkPallindromeForAllDateFormats(date);
+
+    if(isPallindrome) {
+      resultRef.innerHTML = "Yay your birthday is a Pallindrome";
+    } else {
+      var [count, nextDate] = getNextPallindromeDate(date);
+      resultRef.innerHTML = `The next Pallindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you missed it by ${count} days!`; 
+    }
+  }
+}
+
+showBtnRef.addEventListener("click", clickHandler);
